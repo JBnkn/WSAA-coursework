f57f111d87005a85cffacdc814ec00ed46beb23a
Joseph Benkanoun <benkanoun.greene@gmail.com>
2025-04-16 22:00:23 +0100

    Assignment 4 initial commit

---
+++ assignments/assignment04-github.py
@@ -0,0 +1,4 @@
+# assignment04-github.py
+# write a program in python that will read a file from a repository
+# the program should then replace all the instances of the text "Andrew" with your name
+# author: Joseph Benkanoun
