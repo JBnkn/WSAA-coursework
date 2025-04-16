6bdec7c6a216cefb421bd2d7415a8ecd2cfbd379
Joseph Benkanoun <benkanoun.greene@gmail.com>
2025-04-16 21:47:07 +0100

    Assignment 3 work

--- assignments/assignment03-cso.py
+++ assignments/assignment03-cso.py
@@ -2,3 +2,8 @@
 # write a program that retrieves the dataset for the "exchequer account (historical series)" from the CSO, and stores it into a file called "cso.json"
 # author: Joseph Benkanoun
 
+import requests
+
+data = requests.get("https://data.cso.ie/table/FIQ02")
+print(data.status_code)
+print(data)
