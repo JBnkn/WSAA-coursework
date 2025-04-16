9a1d9924ab8d7774811204230cff84fd73696025
Joseph Benkanoun <benkanoun.greene@gmail.com>
2025-04-16 21:55:05 +0100

    Assignment 3 work

--- assignments/assignment03-cso.py
+++ assignments/assignment03-cso.py
@@ -3,7 +3,12 @@
 # author: Joseph Benkanoun
 
 import requests
+import json
 
-data = requests.get("https://data.cso.ie/table/FIQ02")
-print(data.status_code)
-print(data)
+url = "https://ws.cso.ie/public/api.jsonrpc?data=%7B%22jsonrpc%22:%222.0%22,%22method%22:%22PxStat.Data.Cube_API.ReadDataset%22,%22params%22:%7B%22class%22:%22query%22,%22id%22:%5B%5D,%22dimension%22:%7B%7D,%22extension%22:%7B%22pivot%22:null,%22codes%22:false,%22language%22:%7B%22code%22:%22en%22%7D,%22format%22:%7B%22type%22:%22JSON-stat%22,%22version%22:%222.0%22%7D,%22matrix%22:%22FIQ02%22%7D,%22version%22:%222.0%22%7D%7D"
+
+response = requests.get(url)
+data = response.json()
+
+with open("cso.json", "wt") as fp:
+    print(json.dumps((data)), file=fp)
