263931201583a5be126dbf45d95c07a65d8ac7f4
Joseph Benkanoun <benkanoun.greene@gmail.com>
2025-04-17 08:36:47 +0100

    Assignment 4 final commit

--- assignments/assignment04-github.py
+++ assignments/assignment04-github.py
@@ -6,6 +6,7 @@
 from github import Github
 import requests
 from config import githubkey as cfg
+import base64
 
 url = 'https://api.github.com/repos/JBnkn/misc/contents/'
 
@@ -14,8 +15,23 @@ response = requests.get(url, auth = ('token', key))
 print(response.status_code)
 
 file = requests.get('https://api.github.com/repos/JBnkn/misc/contents/Andrew2Joseph.txt', auth = ('token', key))
-content = file.json()
-print(content)
+print(file.status_code)
 
-# struggling to get this to work
-# Python keeps returning an error when I try to pip install Github
+file_data = file.json()
+
+# asked for advice on my code from ChatGPT and it suggested using to return the file contents
+# I'm not fully getting this put unfortunately am flying out for Easter this morning so don't have time to continue tweaking
+# disappointed with this assignment - it just hasn't clicked for me
+content_base64 = file_data['content']
+content = base64.b64decode(content_base64).decode('utf-8')
+
+updated_content = content.replace('Andrew', 'Joseph')
+
+update_payload = {
+        'message': 'Replace all instances of Andrew with Joseph',
+        'content': base64.b64encode(updated_content.encode('utf-8')).decode('utf-8'),
+        'sha': file_data['sha'] 
+    }
+
+update_response = requests.put('https://api.github.com/repos/JBnkn/misc/contents/Andrew2Joseph.txt', json=update_payload, auth=('token', key))
+print(update_response.status_code)
