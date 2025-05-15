c2413770d996660936e34ba6c4317ec8c26641d7
Joseph Benkanoun <benkanoun.greene@gmail.com>
2025-05-15 16:35:03 +0100

    Removed Project

--- big project/wsaarestserver.py
+++
@@ -1,6 +0,0 @@
-# Write a program that demonstrates that you understand creating and consuming RESTful APIs
-# I will allow a lot of flexibility in this project, so that you can use it as an # opportunity to do something that is useful for your work.
-
-# If you cannot think of a project to do: 
-# Create a Web application in Flask that has a RESTful API, the application # should link to one or more database tables.
-# You should also create the web pages that can consume the API. I.e. performs CRUD operations on the data.
